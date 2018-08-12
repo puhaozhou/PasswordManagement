f2f49ef85d5558f106d7149631a96c1b819146f3
markphz <puhaozhou@163.com>
2018-08-12 16:16:10 +0800

    添加引用

--- PasswordManagement/ViewModels/SiteViewModel.cs
+++ PasswordManagement/ViewModels/SiteViewModel.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PasswordManagement.Business;
+using PasswordManagement.DataAccess.Entity;
 
 namespace PasswordManagement.ViewModels
 {
     public class SiteViewModel : NotificationObject
     {
-        public List<SiteModel> 
+        private List<SiteModel> site_bind;
+        public List<SiteModel> Site_Bind
+        {
+            get
+            {
+                return site_bind;
+            }
+            set
+            {
+                site_bind = value;
+                this.RaisePropertyChanged("Site_Bind"); 
+            }
+        }
+
+        public List<SiteModel> GetSitePassword()
+        {
+            var manager = new PasswordManager();
+            var result = manager.GetSitePassword();
+            return result;
+        }
+
+        public SiteViewModel()
+        {
+            Site_Bind = GetSitePassword();
+        }
     }
 }
