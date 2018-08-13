edbd88cf7449e33956bf29b2e52a4bbdb303454d
markphz <markphz@MARKPHZ-PC>
2018-08-13 18:13:40 +0800

    加入datagrid

--- PasswordManagement/View/SiteInfo.xaml.cs
+++ PasswordManagement/View/SiteInfo.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PasswordManagement.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace PasswordManagement.View
         public SiteInfo()
         {
             InitializeComponent();
+            this.DataContext = new SiteViewModel();
         }
     }
 }
